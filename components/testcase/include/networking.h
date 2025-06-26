eeee7215c0c3f7c22c42b5c2ac45ef132a99b152
Dylan Taft <dylanetaft@gmail.com>
2025-06-26 12:31:21 -0400

    begin netcode

--- components/testcase/include/networking.h
+++ components/testcase/include/networking.h
@@ -1,10 +1,9 @@
 #ifndef WIFI_H
 #define WIFI_H 
-#define DEFAULT_WIFI_SSIS      "MASLOW_CNC"
-#define DEFAULT_ESP_WIFI_PASS      "12345678"
-#define DEFAULT_ESP_WIFI_CHANNEL   CONFIG_ESP_WIFI_CHANNEL
+#define DEFAULT_WIFI_SSID      "MASLOW_CNC"
+#define DEFAULT_WIFI_PASS      "12345678"
 #define DEFAULT_MAX_STA_CONN       8
-#define DEFAULT_ESP_WIFI_CHANNEL      6
+#define DEFAULT_WIFI_CHANNEL      6
 
 void initialize_networking();
 #endif
--- components/testcase/networking.c
+++ components/testcase/networking.c
@@ -9,23 +9,43 @@
 #include "lwip/err.h"
 #include "lwip/sys.h"
 #include "networking.h"
+#include "lwip/err.h"
+#include "lwip/sockets.h"
+#include "lwip/sys.h"
+#include <lwip/netdb.h>
 
 
+#define PORT 23
 
 void initialize_wifi();
+void initialize_socket();
 
+void initialize_socket() {
+        int ip_protocol = IPPROTO_IPV6;
+        int addr_family = AF_INET6; // Use IPv6 address family
+        struct sockaddr_storage dest_addr;
 
+        struct sockaddr_in6 *dest_addr_ip6 = (struct sockaddr_in6 *)&dest_addr;
+        bzero(&dest_addr_ip6->sin6_addr.un, sizeof(dest_addr_ip6->sin6_addr.un));
+        dest_addr_ip6->sin6_family = AF_INET6;
+        dest_addr_ip6->sin6_port = htons(PORT);
+        int listen_sock = socket(addr_family, SOCK_STREAM, ip_protocol);
+        assert(listen_sock >= 0);
+        int no = 0;
+        assert((setsockopt(listen_sock, IPPROTO_IPV6, IPV6_V6ONLY, &no, sizeof(no)) < 0));
+}
 
 void initialize_networking() {
     initialize_wifi();
+    initialize_socket();
 }
 void initialize_wifi() {
   wifi_config_t wifi_config = {
         .ap = {
-            .ssid = DEFAULT_ESP_WIFI_SSID,
-            .ssid_len = strlen(DEFAULT_ESP_WIFI_SSID),
-            .channel = DEFAULT_ESP_WIFI_CHANNEL,
-            .password = DEFAULT_ESP_WIFI_PASS,
+            .ssid = DEFAULT_WIFI_SSID,
+            .ssid_len = strlen(DEFAULT_WIFI_SSID),
+            .channel = DEFAULT_WIFI_CHANNEL,
+            .password = DEFAULT_WIFI_PASS,
             .max_connection = DEFAULT_MAX_STA_CONN,
 #ifdef CONFIG_ESP_WIFI_SOFTAP_SAE_SUPPORT
             .authmode = WIFI_AUTH_WPA3_PSK,
